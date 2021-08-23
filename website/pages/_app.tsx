d4615e481270dcd5d80eacde37b7da039b720984
Haydn <haydn@energiebespaarders.nl>
2021-08-23 17:30:47 +0100

    add wall background

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -6,12 +6,27 @@ import theme from "../styles/theme";
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ width: ["100%", "600px"], m: "auto", p: [1, 0] }}>
-        <Component {...pageProps} />
+      <Box
+        sx={{
+          backgroundImage: 'url("/wall.svg")',
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <Box
+          sx={{
+            width: ["100%", "600px"],
+            m: "auto",
+            p: [1, 0],
+          }}
+        >
+          <Component {...pageProps} />
+        </Box>
+        <footer>
+          <Footer />
+        </footer>
       </Box>
-      <footer>
-        <Footer />
-      </footer>
     </ThemeProvider>
   );
 }
